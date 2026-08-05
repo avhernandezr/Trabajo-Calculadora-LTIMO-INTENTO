f96d730cc711b4039d523c1f22f52ace0c4f4ed0
federica <pepylongueiraviera@gmail.com>
2026-08-05 12:17:40 -0300

    Operacion multiplicacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,13 @@
         Console.WriteLine(Division.Divide(7, 8));
     }
 }
+public class Multiplication
+{
+   public static int Multiply(int a, int b)
+   {
+       return a * b;
+   }
+}
 public class Addition
 {
    public static int Add(int a, int b)
