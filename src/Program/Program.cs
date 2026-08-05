862e4f3cb4db34e3979f8b4dea8a9b1c1033d814
federica <pepylongueiraviera@gmail.com>
2026-08-05 12:30:40 -0300

    ningunncambio

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,11 +8,13 @@
         Console.WriteLine(Division.Divide(7, 8));
     }
 }
-public class Multiplication
+public class Addition
 {
-   public static int Multiply(int a, int b)
+   public static int Add(int a, int b)
    {
-       return a + b;}}
+       return a + b;
+   }
+}
 public class Division
 {
    public static double Divide(int a, int b)
