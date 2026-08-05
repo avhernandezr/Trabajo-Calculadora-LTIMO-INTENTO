b877fbb8ff9da4fed7b4b785017f1445a92b60c9
federica <pepylongueiraviera@gmail.com>
2026-08-05 12:22:16 -0300

    puseLLaves

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -13,10 +13,12 @@ public class Addition
    public static int Add(int a, int b)
    {
        return a + b;
+   }
 public class Division
 {
    public static double Divide(int a, int b)
    {
        return (double)a / b;
    }
+}
 }
